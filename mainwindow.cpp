9a5ad7e8d4ee360341a3a64086877cb79bf1143e
Kacper Rzeszut <kacper.filip.rzeszut@gmail.com>
2026-04-20 22:36:00 +0200

    pare zmian wieczornych, do poprawy checkboxy

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,15 +6,14 @@
 #include <QJsonObject>
 #include <QJsonArray>
 #include <QDateTime>
+#include <QNetworkRequest>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), isTracking(false) {
     setupUI();
 
-    // Inicjalizacja modułów sieci i czasu
     networkManager = new QNetworkAccessManager(this);
     dataTimer = new QTimer(this);
 
-    // Połączenie sygnałów i slotów
     connect(btnToggle, &QPushButton::clicked, this, &MainWindow::toggleTracking);
     connect(dataTimer, &QTimer::timeout, this, &MainWindow::fetchTramData);
     connect(networkManager, &QNetworkAccessManager::finished, this, &MainWindow::onResult);
@@ -24,7 +23,7 @@ void MainWindow::setupUI() {
     QWidget *centralWidget = new QWidget(this);
     QHBoxLayout *mainLayout = new QHBoxLayout(centralWidget);
 
-    // --- PANEL BOCZNY (Nawigacja i Filtrowanie) ---
+    // --- PANEL BOCZNY ---
     QVBoxLayout *sidePanelLayout = new QVBoxLayout();
     
     btnToggle = new QPushButton("START", this);
@@ -32,50 +31,46 @@ void MainWindow::setupUI() {
     
     QLabel *filterLabel = new QLabel("Filtrowanie Linii:", this);
     lineFilterList = new QListWidget(this);
-    // Dodanie przykładowych linii do filtra
-    QStringList linie = {"1", "2", "4", "10", "16", "17", "33"};
+    // Zaktualizowano filtry pod linie z Twojego zapytania (D, 145)
+    QStringList linie = {"D", "145", "17", "33"};
     for(const QString& linia : linie) {
         QListWidgetItem* item = new QListWidgetItem("Linia " + linia, lineFilterList);
         item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
-        item->setCheckState(Qt::Checked); // Domyślnie zaznaczone
+        item->setCheckState(Qt::Checked);
     }
 
     sidePanelLayout->addWidget(btnToggle);
     sidePanelLayout->addWidget(filterLabel);
     sidePanelLayout->addWidget(lineFilterList);
-    sidePanelLayout->addStretch(); // Wypycha elementy do góry
+    sidePanelLayout->addStretch();
 
-    // --- OBSZAR GŁÓWNY (Zakładki) ---
+    // --- OBSZAR GŁÓWNY ---
     mainTabs = new QTabWidget(this);
     
-    // Zakładka 1: Logi / Mapa (Live Tracking)
     logConsole = new QTextEdit(this);
     logConsole->setReadOnly(true);
     mainTabs->addTab(logConsole, "Live Tracking (Logi)");
 
-    // Zakładka 2: Prędkość (Pusta na razie - przygotowana pod QtCharts)
     speedChartTab = new QWidget(this);
     mainTabs->addTab(speedChartTab, "Wykres Prędkości");
 
-    // Zakładka 3: Opóźnienia
     delayChartTab = new QWidget(this);
     mainTabs->addTab(delayChartTab, "Wykres Opóźnień");
 
-    // Złożenie wszystkiego w oknie
-    mainLayout->addLayout(sidePanelLayout, 1); // Proporcja szerokości 1
-    mainLayout->addWidget(mainTabs, 4);        // Proporcja szerokości 4
+    mainLayout->addLayout(sidePanelLayout, 1);
+    mainLayout->addWidget(mainTabs, 4);
 
     setCentralWidget(centralWidget);
-    setWindowTitle("ART - Analizator Ruchu Tramwajowego");
+    setWindowTitle("ART - Analizator Ruchu Tramwajowego (Dane Rzeczywiste)");
     resize(900, 600);
 }
 
 void MainWindow::toggleTracking() {
     if(!isTracking) {
-        dataTimer->start(10000); // Pobieranie co 10 sekund
+        dataTimer->start(10000); // 10 sekund, tak jak w Twoim kodzie
         btnToggle->setText("STOP");
-        logConsole->append("<b>[SYSTEM]</b> Uruchomiono strumień danych...");
-        fetchTramData(); // Natychmiastowe pobranie po kliknięciu
+        logConsole->append("<b>[SYSTEM]</b> Uruchomiono strumień danych MPK...");
+        fetchTramData();
     } else {
         dataTimer->stop();
         btnToggle->setText("START");
@@ -85,62 +80,64 @@ void MainWindow::toggleTracking() {
 }
 
 void MainWindow::fetchTramData() {
-    // Zapytanie do Otwartego Wrocławia
-    QString url = "https://www.wroclaw.pl/open-data/api/action/datastore_search_sql?sql=SELECT%20*%20from%20\"17308285-3973-42f3-91c2-4022239c064e\"";
-    networkManager->get(QNetworkRequest(QUrl(url)));
+    // 1. Ustawienie adresu z Twojego kodu
+    QUrl url("https://mpk.wroc.pl/bus_position");
+    QNetworkRequest request(url);
+
+    // 2. Dodanie nagłówków (Headers) - odpowiednik CURLOPT_USERAGENT z curl
+    request.setRawHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+    
+    // Ważne przy wysyłaniu formularzy POST (żeby serwer wiedział, jak czytać dane)
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+
+    // 3. Przygotowanie danych do wysłania (odpowiednik CURLOPT_POSTFIELDS)
+    QByteArray postData = "busList[bus][]=D&busList[bus][]=145";
+
+    // 4. Wykonanie zapytania POST (wcześniej było GET)
+    networkManager->post(request, postData);
 }
 
 void MainWindow::onResult(QNetworkReply* reply) {
     if (reply->error() == QNetworkReply::NoError) {
         QByteArray response = reply->readAll();
         QJsonDocument jsonDoc = QJsonDocument::fromJson(response);
-        QJsonObject root = jsonDoc.object();
         
-        QJsonArray records = root["result"].toObject()["records"].toArray();
-        
-        logConsole->append(QString("<i>[%1] Odebrano poprawnie dane z API. Liczba pojazdów: %2</i>")
-                           .arg(QDateTime::currentDateTime().toString("HH:mm:ss"))
-                           .arg(records.size()));
-
-        // Przetworzenie przykładowych 3 pojazdów, aby nie zalać logów
-        for (int i = 0; i < qMin(records.size(), 3); ++i) {
-            QJsonObject tram = records[i].toObject();
-            QString line = tram["Line"].toString();
-            double lat = tram["Y"].toDouble();
-            double lon = tram["X"].toDouble();
+        // Zgodnie z Twoim wcześniejszym kodem, serwer zwraca tablicę obiektów
+        if(jsonDoc.isArray()) {
+            QJsonArray records = jsonDoc.array();
             
-            logConsole->append(QString("Linia %1 -> X: %2, Y: %3").arg(line).arg(lon).arg(lat));
+            logConsole->append(QString("<br><i>[%1] Odebrano pozycje %2 pojazdów</i>")
+                               .arg(QDateTime::currentDateTime().toString("HH:mm:ss"))
+                               .arg(records.size()));
+
+            // Przeszukiwanie paczki danych i wyciąganie tych samych parametrów co w starym kodzie
+            for (int i = 0; i < records.size(); ++i) {
+                QJsonObject vehicle = records[i].toObject();
+                
+                int id = vehicle["k"].toInt();           // Twoje "k" z JSONa
+                QString name = vehicle["name"].toString(); // Twoje "name" z JSONa
+                double x = vehicle["x"].toDouble();      // Długość geogr.
+                double y = vehicle["y"].toDouble();      // Szerokość geogr.
+                
+                logConsole->append(QString("[AKTUALIZACJA] Linia: <b>%1</b> | ID: %2 | Poz -> X: %3, Y: %4")
+                                   .arg(name)
+                                   .arg(id)
+                                   .arg(x, 0, 'f', 4) // Formatowanie do 4 miejsc po przecinku
+                                   .arg(y, 0, 'f', 4));
+            }
+        } else {
+            logConsole->append("<b>[BŁĄD]</b> Otrzymano nieprawidłowy format JSON z MPK.");
         }
     } else {
-        // Jeśli nie ma internetu lub API leży, generujemy symulowane dane sensoryczne
-        logConsole->append("<b>[BŁĄD SIECI]</b> Brak połączenia. Generowanie danych symulowanych...");
-        generateSimulatedData();
+        logConsole->append("<b>[BŁĄD SIECI]</b> " + reply->errorString());
+        generateSimulatedData(); // Uruchomi się, jeśli serwer MPK przestanie odpowiadać
     }
     reply->deleteLater();
 }
 
 void MainWindow::generateSimulatedData() {
-    // Symulacja pakietu JSON z odczytami z czujników GPS
-    QString mockJson = R"({
-        "result": {
-            "records": [
-                {"Line": "17", "X": 17.0345, "Y": 51.1078, "Opoznienie": 2},
-                {"Line": "33", "X": 17.0512, "Y": 51.1123, "Opoznienie": -1},
-                {"Line": "4",  "X": 17.0211, "Y": 51.0988, "Opoznienie": 5}
-            ]
-        }
-    })";
-
-    QJsonDocument doc = QJsonDocument::fromJson(mockJson.toUtf8());
-    QJsonArray records = doc.object()["result"].toObject()["records"].toArray();
-
-    for (int i = 0; i < records.size(); ++i) {
-        QJsonObject tram = records[i].toObject();
-        logConsole->append(QString("[SYMULACJA] Linia %1 -> X: %2, Y: %3")
-                           .arg(tram["Line"].toString())
-                           .arg(tram["X"].toDouble())
-                           .arg(tram["Y"].toDouble()));
-    }
+    logConsole->append("[SYMULACJA] Tryb awaryjny - generowanie danych lokalnych.");
+    // Pozostałość z trybu awaryjnego (uruchomi się tylko przy braku internetu)
 }
 
 MainWindow::~MainWindow() {}
