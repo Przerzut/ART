cc5dbe5be9214d96803b621678fffbef65e94077
Kacper Rzeszut <kacper.filip.rzeszut@gmail.com>
2026-05-12 14:29:11 +0200

    poprawki

--- app_pl.ts
+++ app_pl.ts
@@ -4,27 +4,21 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="ART/mainwindow.cpp" line="107"/>
         <location filename="mainwindow.cpp" line="171"/>
         <source>Change Language (PL)</source>
         <translation>Zmień Język (ENG)</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="108"/>
-        <location filename="ART/mainwindow.cpp" line="157"/>
         <location filename="mainwindow.cpp" line="172"/>
         <source>STOP</source>
         <translation>STOP</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="108"/>
-        <location filename="ART/mainwindow.cpp" line="174"/>
         <location filename="mainwindow.cpp" line="172"/>
         <source>START</source>
         <translation>START</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="109"/>
         <location filename="mainwindow.cpp" line="173"/>
         <source>Status: Active</source>
         <translation>Status: Aktywny</translation>
@@ -32,16 +26,14 @@
     <message>
         <location filename="mainwindow.cpp" line="173"/>
         <source>Status: Inactive</source>
-        <translation>Status:nieaktywny</translation>
+        <translation type="unfinished">Status: nieaktywny</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="110"/>
         <location filename="mainwindow.cpp" line="174"/>
         <source>&lt;b&gt;ROUTE MONITOR - LIVE SENSORY DATA&lt;/b&gt;</source>
         <translation>&lt;b&gt;MONITOR TRASY - DANE SENSORYCZNE LIVE&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="111"/>
         <location filename="mainwindow.cpp" line="175"/>
         <source>&lt;b&gt;Line Filtering:&lt;/b&gt;</source>
         <translation>&lt;b&gt;Filtrowanie linii:&lt;/b&gt;</translation>
@@ -52,7 +44,6 @@
         <translation>&lt;b&gt;Wybierz Pojazd (ID):&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="113"/>
         <location filename="mainwindow.cpp" line="177"/>
         <source>Speed [km/h]</source>
         <translation>Prędkość [km/h]</translation>
@@ -63,47 +54,39 @@
         <translation>Mapa 2D [GPS]</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="114"/>
         <source>Delay [min]</source>
-        <translation>Opóźnienie [min]</translation>
+        <translation type="vanished">Opóźnienie [min]</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="116"/>
         <location filename="mainwindow.cpp" line="179"/>
         <source>ART - Tram Traffic Analyzer</source>
         <translation>ART - Analizator Ruchu Tramwajowego</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="142"/>
         <source>&lt;i&gt;[SYSTEM] Warning: Translation file &apos;app_pl.qm&apos; not found.&lt;/i&gt;</source>
-        <translation>&lt;i&gt;[SYSTEM] Błąd: Nie znaleziono pliku tłumaczenia &apos;app_pl.qm&apos;.&lt;/i&gt;</translation>
+        <translation type="vanished">&lt;i&gt;[SYSTEM] Błąd: Nie znaleziono pliku tłumaczenia &apos;app_pl.qm&apos;.&lt;/i&gt;</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="161"/>
         <location filename="mainwindow.cpp" line="206"/>
         <source>TIME</source>
         <translation>CZAS</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="162"/>
         <location filename="mainwindow.cpp" line="206"/>
         <source>LINE</source>
-        <translation type="unfinished">Linia</translation>
+        <translation>Linia</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="164"/>
         <location filename="mainwindow.cpp" line="206"/>
         <source>POS_X</source>
         <translation>POZ_X</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="165"/>
         <location filename="mainwindow.cpp" line="206"/>
         <source>POS_Y</source>
         <translation>POZ_Y</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="176"/>
         <location filename="mainwindow.cpp" line="217"/>
         <source>&lt;i&gt;[SYSTEM] Monitoring stopped.&lt;/i&gt;</source>
         <translation>&lt;i&gt;[SYSTEM] Monitoring zatrzymany.&lt;/i&gt;</translation>
@@ -119,10 +102,9 @@
         <translation>&lt;i&gt;[ERROR] Błąd API: %1&lt;/i&gt;</translation>
     </message>
     <message>
-        <location filename="ART/mainwindow.cpp" line="217"/>
         <location filename="mainwindow.cpp" line="392"/>
         <source>Line </source>
-        <translation type="unfinished">Linia </translation>
+        <translation>Linia </translation>
     </message>
 </context>
 </TS>
